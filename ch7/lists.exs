2ef2502dd591934b07733806e89dba1bedd58081
MiszaZG <michalzejmo@gmail.com>
2017-06-26 20:48:28 +0200

    ch7 max

--- ch7/lists.exs
+++ ch7/lists.exs
@@ -11,6 +11,15 @@ defmodule MyList do
 	def map([], _func), do: []
 	def map([head | tail], func), do: [func.(head) | map(tail, func)]
 
+	def reduce([], value, _), do: value
+	def reduce([head | tail], value, fun) do 
+		reduce(tail, fun.(head, value), fun)
+	end
+
+	def mapsum(list, fun) do
+		map(list, fun) |> reduce(0, &(&1 + &2))
+	end
+
 	def sum(list), do: _sum(list, 0)
 	defp _sum([], total), do: total
 	defp _sum([head | tail], total), do: _sum(tail, head + total)
@@ -19,6 +28,11 @@ defmodule MyList do
 	def sum3([head | []]), do: head
 	def sum3([head | [head2 | tail]]), do: sum3([head + head2 | tail])
 
+	def mmax([]), do: []
+	def mmax([head | tail]), do: _mmax(tail, head)
+	defp _mmax([], value), do: value
+	defp _mmax([head | tail], value) when (value > head), do: _mmax(tail, value)
+	defp _mmax([head | tail], value) when (value < head), do: _mmax(tail, head)
 end
 
 
@@ -36,3 +50,8 @@ IO.puts("sum3")
 IO.inspect(MyList.sum3([]))
 IO.inspect(MyList.sum3([1, 2, 10, 20]))
 
+IO.puts("mapsum")
+IO.inspect(MyList.mapsum([1, 2, 3], &(&1 * &1)))
+
+IO.inspect(MyList.mmax([]))
+IO.inspect(MyList.mmax([17, 2, 3]))
