10be15ecf57b3ebb07108e40299c1fb388d5b754
MiszaZG <michalzejmo@gmail.com>
2017-06-30 13:08:01 +0200

    map

--- ch8/map_key_sets.exs
+++ ch8/map_key_sets.exs
@@ -12,3 +12,33 @@ defmodule Canvas do
 end
 
 Canvas.draw_text("check check", style: "style2", font: "Arial")
+
+
+people = [
+	%{ name: "Grumpy", height: 1.91 },
+	%{ name: "Patty", height: 1.25 },
+	%{ name: "Pi", height: 1.81 }
+]
+
+IO.inspect(for person = %{ height: height } <- people, height > 1.7, 
+	do: person)
+
+
+defmodule HotelRoom do
+	
+	def book(%{name: name, height: height})
+	when height > 1.9 do
+		IO.puts("Need extra long bed for #{name}")
+	end
+
+	def book(%{name: name, height: height})
+	when height < 1.3 do
+		IO.puts("Low shower")
+	end
+
+	def book(person) do
+		IO.puts("Regular")
+	end
+end
+
+people |> Enum.each(&HotelRoom.book/1)
